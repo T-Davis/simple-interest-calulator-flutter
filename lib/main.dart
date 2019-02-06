bb6dea5243e4498bcdcd72e86f3dfa815ccf855f
Trevor Davis <tzendou@gmail.com>
2019-02-06 10:16:18 -0800

    "add text fields and currency dropdown menu"

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,42 @@ class _SIFormState extends State<SIForm> {
       body: Container(
         margin: EdgeInsets.all(_minimumPadding * 2),
         child: Column(
-          children: <Widget>[getBankImage()],
+          children: <Widget>[
+            getBankImage(),
+            Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: textField('Principal', 'Enter Principal e.g. 12000')),
+            Padding(
+                padding: EdgeInsets.only(
+                    top: _minimumPadding, bottom: _minimumPadding),
+                child: textField('Rate of Interest', 'In percent')),
+            Padding(
+              padding: EdgeInsets.only(
+                  top: _minimumPadding, bottom: _minimumPadding),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: textField('Term', 'Time in years'),
+                  ),
+                  Container(
+                    width: _minimumPadding * 5,
+                  ),
+                  Expanded(
+                      child: DropdownButton<String>(
+                        items: _currencies.map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        value: 'Dollars',
+                        onChanged: (String newValueSelected) {},
+                      ))
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
@@ -40,5 +75,15 @@ class _SIFormState extends State<SIForm> {
           height: 125.0,
         ),
         margin: EdgeInsets.all(_minimumPadding * 10),
+      );
+
+  Widget textField(String label, String hint) =>
+      TextField(
+        keyboardType: TextInputType.number,
+        decoration: InputDecoration(
+            labelText: label,
+            hintText: hint,
+            border:
+            OutlineInputBorder(borderRadius: BorderRadius.circular(5.0))),
       );
 }
