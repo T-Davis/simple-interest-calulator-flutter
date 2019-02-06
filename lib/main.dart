19c8159118ac3729d90f95357d60227f51ce08cc
Trevor Davis <tzendou@gmail.com>
2019-02-05 22:00:08 -0800

    "add bank image and margin"

--- lib/main.dart
+++ lib/main.dart
@@ -15,16 +15,30 @@ class SIForm extends StatefulWidget {
 
 class _SIFormState extends State<SIForm> {
   var _currencies = ['Dollars', 'Pounds', 'Rupees'];
+  final _minimumPadding = 5.0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Simple Interest Calculator'),
-      ), body: Container(
-      child: Column(children: <Widget>[
-      ],),
-    ),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(_minimumPadding * 2),
+        child: Column(
+          children: <Widget>[getBankImage()],
+        ),
+      ),
     );
   }
+
+  Widget getBankImage() =>
+      Container(
+        child: Image(
+          image: AssetImage('images/bank.png'),
+          width: 125.0,
+          height: 125.0,
+        ),
+        margin: EdgeInsets.all(_minimumPadding * 10),
+      );
 }
